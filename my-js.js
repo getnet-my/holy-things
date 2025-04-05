306130fdb9f566c8c69a0e4b4c78b49908cadf2d
getnet-my <adetketema173@gmail.com>
2025-04-05 23:47:14 +0300

    Add files via upload

---
+++ my-js.js
@@ -0,0 +1,48 @@
+//const product=['1','2','3','4',];
+
+   
+   let addall= `<div class="myk">
+   <img src="all/images (1).jpeg" alt="no-picture-found">
+   <p>Eotc-Holy-picture</p>
+   </div>` ;
+   let bt;
+   let all;
+
+   
+for(let i=2;i<125;i++){
+    all="jpeg";
+
+    if(i===14 || i===15){
+        continue;
+    }else if(i===16 || i===17){
+        continue;
+    }else if(i===18 || i===21){
+        continue;
+
+    }else if(i===58 || i===59){
+           continue;
+    }else if(i>63){
+        all="jpg";
+            
+    }
+
+
+           
+
+           
+    addall+=
+           `<div class="myk">
+            <img controls src="all/images (${i}).${all}" alt="no-picture-found">
+            <p>Eotc-Holy-picture</p>
+            </div>`          
+            ;
+           
+};
+
+
+
+document.querySelector('.myk2').innerHTML=addall;
+
+    
+
+
